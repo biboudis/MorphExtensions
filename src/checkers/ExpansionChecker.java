d23ae1e0d07d818bdbfc1c6cf63756246b287535
Aggelos Biboudis <biboudis@gmail.com>
2013-12-25 16:01:12 +0200

    Commit recent changes prior to renaming.

--- src/checkers/ExpansionChecker.java
+++ src/checkers/ExpansionChecker.java
@@ -8,16 +8,24 @@ import com.sun.tools.javac.tree.JCTree;
 import annotations.For;
 import checkers.basetype.BaseTypeChecker;
 import checkers.quals.TypeQualifiers;
+import checkers.source.SourceChecker;
+import checkers.source.SourceVisitor;
 import expanders.StaticForMethodExpander;
 
 @TypeQualifiers({For.class})
-public class ExpansionChecker extends BaseTypeChecker {
+public class ExpansionChecker extends SourceChecker {
 
 	@Override
 	public void typeProcess(TypeElement elem, TreePath path) {
 		JCTree tree = (JCTree) path.getCompilationUnit();
 		
 		tree.accept(new StaticForMethodExpander(processingEnv, path));
+	}
+
+	@Override
+	protected SourceVisitor<?, ?> createSourceVisitor() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
