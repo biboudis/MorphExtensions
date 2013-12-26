6d3efdf8a4dfbd713d345b09d6fa76106b5be449
Aggelos Biboudis <biboudis@gmail.com>
2013-12-26 19:21:52 +0200

    Changed structure.

--- src/checkers/MorphChecker.java
+++ src/checkers/MorphChecker.java
@@ -2,23 +2,29 @@ package checkers;
 
 import javax.lang.model.element.TypeElement;
 
+import visitors.InstantiationTranslator;
+
 import com.sun.source.util.TreePath;
 import com.sun.tools.javac.tree.JCTree;
 
 import annotations.For;
+import annotations.Morph;
 import checkers.basetype.BaseTypeChecker;
 import checkers.quals.TypeQualifiers;
 import checkers.source.SourceChecker;
 import checkers.source.SourceVisitor;
-import expanders.StaticForMethodExpander;
 
-@TypeQualifiers({For.class})
-public class ForChecker extends SourceChecker {
+@TypeQualifiers({Morph.class})
+public class MorphChecker extends SourceChecker {
 
 	@Override
 	public void typeProcess(TypeElement elem, TreePath path) {
+		
+		super.typeProcess(elem, path);
+
 		JCTree tree = (JCTree) path.getCompilationUnit();		
-		tree.accept(new StaticForMethodExpander(processingEnv, path));
+		
+		tree.accept(new InstantiationTranslator(processingEnv, path));
 	}
 
 	@Override
---
+++ src/visitors/InstantiationTranslator.java
@@ -0,0 +1,15 @@
+package visitors;
+
+import javax.annotation.processing.ProcessingEnvironment;
+
+import com.sun.source.util.TreePath;
+import com.sun.tools.javac.tree.TreeTranslator;
+
+public class InstantiationTranslator extends TreeTranslator {
+
+	public InstantiationTranslator(ProcessingEnvironment processingEnv,
+			TreePath path) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
