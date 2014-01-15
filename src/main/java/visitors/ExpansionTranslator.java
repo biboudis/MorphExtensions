e897f291a1bd3448c18d4d05b12afd7d43224bfb
Aggelos Biboudis <biboudis@gmail.com>
2014-01-15 19:14:31 +0200

    Proper AST, but something is attributed wrongly.

--- src/main/java/visitors/ExpansionTranslator.java
+++ src/main/java/visitors/ExpansionTranslator.java
@@ -7,15 +7,22 @@ import checkers.types.AnnotatedTypeFactory;
 
 import com.sun.source.util.TreePath;
 import com.sun.tools.javac.code.Scope;
+import com.sun.tools.javac.code.Symbol.MethodSymbol;
+import com.sun.tools.javac.code.Symtab;
+import com.sun.tools.javac.code.Type;
 import com.sun.tools.javac.comp.Attr;
 import com.sun.tools.javac.comp.Enter;
 import com.sun.tools.javac.comp.MemberEnter;
+import com.sun.tools.javac.comp.Resolve;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.tree.JCTree.*;
+import com.sun.tools.javac.tree.JCTree.JCExpression;
 import com.sun.tools.javac.tree.TreeInfo;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.tree.TreeTranslator;
 import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.List;
+import com.sun.tools.javac.util.Name;
 import com.sun.tools.javac.util.Names;
 
 public class ExpansionTranslator extends TreeTranslator {
@@ -23,13 +30,15 @@ public class ExpansionTranslator extends TreeTranslator {
 	protected static final String SYN_PREFIX = "__";
 	
 	protected Context context;
-	//private Symtab syms;
-	protected TreeMaker maker;
+	private Symtab syms;
+	protected TreeMaker make;
 	protected Names names;
 	protected Enter enter;
+    private Resolve rs;
 	protected MemberEnter memberEnter;
 	protected TreePath path;
 	protected Attr attr;
+	
 	protected AnnotatedTypeFactory atypeFactory;
 	
 	public ExpansionTranslator(
@@ -37,12 +46,15 @@ public class ExpansionTranslator extends TreeTranslator {
 			TreePath path) {
 		
         context = ((JavacProcessingEnvironment) processingEnv).getContext();
-        //syms = Symtab.instance(context);
-        maker = TreeMaker.instance(context);
+        syms = Symtab.instance(context);
+        make = TreeMaker.instance(context);
         names = Names.instance(context);
         enter = Enter.instance(context);
+        rs = Resolve.instance(context);
         memberEnter = MemberEnter.instance(context);
         attr = Attr.instance(context);	
+        
+        context.dump();
 	}
 	
 	@Override
@@ -51,10 +63,29 @@ public class ExpansionTranslator extends TreeTranslator {
 		super.visitVarDef(tree);
 		
 		if (tree.getType().type.tsym.getAnnotation(Morph.class) != null) {
-			System.out.println("# Tree:");
 			
-			System.out.println(tree.getInitializer().type.tsym.isStatic());
-	
+			System.out.println("# tree: " + tree);
+			
+			if (tree.init.getTag() == Tag.NEWCLASS)
+			{
+				List<JCExpression> oldInitializerList = ((JCNewClass) tree.init).args;
+				
+				Name dummyName = names.fromString("__Logged$Stack");
+				
+				Type clazz = new Scope(tree.sym).lookup(dummyName).sym.type;
+				
+				JCNewClass newClassExpression = make.NewClass(null, null,  make.QualIdent(clazz.tsym), oldInitializerList, null);
+				
+				System.out.println("# newClassExpression: " + newClassExpression);
+				
+				JCVariableDecl newVarDef = make.VarDef(tree.mods, tree.name, make.QualIdent(clazz.tsym), newClassExpression);
+				
+				System.out.println("# newVarDef: " + newVarDef);
+				
+				result = newVarDef;
+			}
+			
+
 //			Name dummySyntheticClass = names.fromString("__Logged$Stack");
 //			
 //			System.out.println(new Scope(tree.sym).lookup(tree.name).isStaticallyImported());
@@ -73,13 +104,13 @@ public class ExpansionTranslator extends TreeTranslator {
 			
 		}
 	}
-	
+		
 	public JCExpression makeDotExpression(String chain) {
         String[] symbols = chain.split("\\.");
-        JCExpression node = maker.Ident(names.fromString(symbols[0]));
+        JCExpression node = make.Ident(names.fromString(symbols[0]));
         for (int i = 1; i < symbols.length; i++) {
             com.sun.tools.javac.util.Name nextName = names.fromString(symbols[i]);
-            node = maker.Select(node, nextName);
+            node = make.Select(node, nextName);
         }
         return node;
     }
